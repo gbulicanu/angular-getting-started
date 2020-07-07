9f672e4b89b967bce4df37613bf20de710d75383
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-07-07 10:07:00 +0300

    Remove default as it is passed from container comp

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -6,7 +6,7 @@ import { Component, OnChanges, Input } from "@angular/core";
   styleUrls: ["./star.component.css"],
 })
 export class StarComponent implements OnChanges {
-  @Input() rating: number = 4;
+  @Input() rating: number;
   starWidth: number;
 
   ngOnChanges(): void {
