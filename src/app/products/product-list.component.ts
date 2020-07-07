5a8d620b732dd267c57e026f4ac65c883663ea7a
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-07-07 10:22:41 +0300

    Pass dat from a component using @Output

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -65,4 +65,8 @@ export class ProductListComponent implements OnInit {
         product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
     );
   }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List: " + message;
+  }
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnChanges, Input } from "@angular/core";
+import {
+  Component,
+  OnChanges,
+  Input,
+  EventEmitter,
+  Output,
+} from "@angular/core";
 
 @Component({
   selector: "pm-star",
@@ -8,8 +14,13 @@ import { Component, OnChanges, Input } from "@angular/core";
 export class StarComponent implements OnChanges {
   @Input() rating: number;
   starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.starWidth = (this.rating * 75) / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The ratting ${this.rating} was clicked!`);
   }
 }
